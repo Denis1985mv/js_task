7eb1a7670f215fbdaebb94a40bbe275a494394eb
Dzianis <dzianis888mv@gmail.com>
2022-10-22 22:27:05 +0300

    task_7

--- js/js.js
+++ js/js.js
@@ -1,1 +1,12 @@
-obj2.myFunction(obj1);
+const stringToNumber = function (value) {
+  if (value === "") {
+    return 0;
+  } else {
+    let result = parseInt(value);
+    value = result;
+    console.log(value);
+    return value;
+  }
+};
+
+export default stringToNumber;
