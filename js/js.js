16925b2957c9b3d0b1efea429deb13067cf4cad3
Dzianis <dzianis888mv@gmail.com>
2022-10-27 00:00:57 +0300

    Первый варіант

--- js/js.js
+++ js/js.js
@@ -1,20 +1,56 @@
-
-function price(travelOne) {
-  //console.log(travelOne);
-  let result = `Рсчет с учетом программы лояльности: `;
-  return function (preson1) {
-   // console.log(travelOne);
-   // console.log(preson1);
-   
-    result = (`${result}, скидка  для постоянного клиента ${
-      travelOne.name
-    } составила ${preson1.personal} % -  , 
-         полная стоимость тура ${preson1.amount}, c учетом скидки ${
-      preson1.amount * (1 - Number(preson1.personal) / 100)
-    }`);
-    console.log(result);
-    return result;
-  };
+let calculator = {
+  personal: function (value) {
+      let result = `Рсчет с учетом программы лояльности: `;
+      return function (preson1) {        
+        result = (`${result}, скидка  для постоянного клиента ${
+          travel.One
+        } составила ${preson1.personal} % -  , 
+             полная стоимость тура ${preson1.amount}, c учетом скидки ${
+          preson1.amount * (1 - Number(preson1.personal) / 100)
+        }`);
+        console.log(result);
+        return result;
+      };
+    },
+  regional: function (value) {
+      let result = `Рсчет с учетом программы лояльности: `;
+      return function (preson1) {  
+        result = (`${result}, скидка  для постоянного клиента ${
+          travel.Two
+        } составила ${preson1.regional} % -  , 
+             полная стоимость тура ${preson1.amount}, c учетом скидки ${
+          preson1.amount * (1 - Number(preson1.regional) / 100)
+        }`);
+        console.log(result);
+        return result;
+      };
+    },
+    quantitative: function (value) {
+      let result = `Рсчет с учетом программы лояльности: `;
+      return function (preson1) {  
+        result = (`${result}, скидка  для постоянного клиента ${
+          travel.Three
+        } составила ${preson1.quantitative} % -  , 
+             полная стоимость тура ${preson1.amount}, c учетом скидки ${
+          preson1.amount * (1 - Number(preson1.quantitative) / 100)
+        }`);
+        console.log(result);
+        return result;
+      };
+    },
+    full: function (value) {
+      let result = `Рсчет с учетом программы лояльности: `;
+      return function (preson1) {  
+        result = (`${result}, суммарная скидка для постоянного клиента ${
+          travel.Fourth
+        } составила ${preson1.quantitative} + ${preson1.regional} + ${preson1.personal} % -  , 
+             полная стоимость тура ${preson1.amount}, c учетом скидки ${
+          ((preson1.amount * (1 - Number(preson1.quantitative) / 100)) * (1 - Number(preson1.regional) / 100)) * (1 - Number(preson1.personal) / 100)
+        }`);
+        console.log(result);
+        return result;
+      };
+    },
 }
 
 let preson1 = {
@@ -24,13 +60,24 @@ let preson1 = {
   quantitative: "15",
 };
 
-let travelOne = {
-  name: "Турист 1",
+let travel = {
+  One: "Турист 1",
+  Two: "Турист 2",
+  Three: "Турист 3",
+  Fourth:"Турист 4",
 };
 
-let result = price(travelOne)(preson1);
-//let result = price.call(travelOne, preson1);
-//let result = price.call()(preson1);
-document.write(result);
-console.log(result);
-//let result = price()();
+let travelFirst = calculator.personal(travel.One)(preson1);
+let travelSecond = calculator.regional(travel.Two)(preson1);
+let travelThird = calculator.quantitative(travel.Three)(preson1);
+let travelFourth = calculator.full(travel.Fourth)(preson1);
+
+console.log(travelFirst);
+console.log(travelSecond);
+console.log(travelThird);
+console.log(travelFourth);
+
+document.write(travelFirst, "<br>");
+document.write(travelSecond, "<br>");
+document.write(travelThird, "<br>");
+document.write(travelFourth, "<br>");
